0f8eb04c36ce3612550332ec677a590707e2a3cf
Ameet T <ameetst@gmail.com>
2025-05-02 15:34:31 +0530

    1534.020525

--- Dual Momentum Strategy/dual_momo.py
+++ Dual Momentum Strategy/dual_momo.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import date
+import yfinance as yf
 
+etf_one = "JUNIORBEES.NS"
+etf_two = "GOLDBEES.NS"
 
+# Get data for the ticker
+data = yf.download(etf_one, start="2023-01-01")
+
+# Display the first few rows
+print(data.head())
