b9b50a0d62c98643d40c545e1529950dfffcaa9d
Ameet T <ameetst@gmail.com>
2026-07-14 12:25:23 +0530

    140726.1220

--- yfinance data scripts/fetch_historical_nav.py
+++ yfinance data scripts/fetch_historical_nav.py
@@ -23,7 +23,7 @@ from pathlib import Path
 # ─────────────────────────────────────────────────
 # CONFIG
 # ─────────────────────────────────────────────────
-EXCEL_FILE   = r"C:\Users\ameet\Documents\Github\code\momentum\ETFs\backtest results\ETF - Backtest  - Copy.xlsx"
+EXCEL_FILE   = r"C:\Users\ameet\Documents\Github\code\momentum\ETFs\backtest results\ETF - Backtest  - NIFTY500.xlsx"
 SHEET_NAME   = "DATA"
 START_DATE   = "2020-04-01"           # Fetch from this date
 END_DATE     = datetime.today().strftime("%Y-%m-%d")
